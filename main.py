6abc1b0b73e7e324f3e4a4ebd8d2b57da3705c6f
XYXA05 <bogdansavi05@gmail.com>
2024-07-29 04:13:24 +0200

    update0

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://usskkwk.mark-build.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
