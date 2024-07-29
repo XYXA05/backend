860c84b5ffb481369d5d18c564f7a35ceaece54e
XYXA05 <bogdansavi05@gmail.com>
2024-07-29 09:39:05 +0200

    fwsdv

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost/test?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:GRO41iFNZsp9lS7OidOEafx5Y6GPD1S2Guu6Q279mJGBy7JYeoT14EffDarZL5f6@167.71.42.178:5432/default?charset=utf8mb4"
 
 
 engine = create_engine(
