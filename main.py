4258426c50302504035d8f11968d846444aab85f
XYXA05 <bogdansavi05@gmail.com>
2024-08-06 02:52:26 +0200

    rehd

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from  router import router
 app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 origins = [
-    "https://www.mark-build.com",  # Angular frontend origin
     "https://b4kg48k.mark-build.com",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
